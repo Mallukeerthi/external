93459b9e431ab79efba0eeb7452f0bf835da36f8
Mallukeerthi <117076788+Mallukeerthi@users.noreply.github.com>
2025-07-26 10:57:38 +0530

    Add files via upload

---
+++ LoopDemo1.java
@@ -0,0 +1,16 @@
+class LoopDemo1
+{
+public static void main(string args[])
+{
+int sum=0;
+int avg=0;
+for(int i=1;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum"+sum);
+svg=sum/10;
+System.out.println("avg"+avg);
+}
+}
+
